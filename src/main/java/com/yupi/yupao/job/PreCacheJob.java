267d14dccc7e06d8cba6d16ed9575dc719c51d4b
砂 流 <1018765366@qq.com>
2023-05-29 18:45:23 +0800

    第九期 添加分布式锁 第八期 add 定时任务完成缓存预热推荐用户，并

---
+++ src/main/java/com/yupi/yupao/job/PreCacheJob.java
@@ -0,0 +1,70 @@
+package com.yupi.yupao.job;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yupi.yupao.model.domain.User;
+import com.yupi.yupao.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 缓存预热任务
+ *
+ * @author yupi
+ */
+@Component
+@Slf4j
+public class PreCacheJob {
+
+    @Resource
+    private UserService userService;
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+    @Resource
+    private RedissonClient redissonClient;
+
+    // 重点用户
+    private List<Long> mainUserList = Arrays.asList(1L);
+
+    // 每天执行，预热推荐用户  "秒 分 时 日 月 星期"
+    @Scheduled(cron = "0 38 6 * * *")
+    public void doCacheRecommendUser() {
+        RLock lock = redissonClient.getLock("yupao:precachejob:docache:lock ");
+        try {
+            if (lock.tryLock(0, -1, TimeUnit.MILLISECONDS)) {
+                System.out.println("getLock: " + Thread.currentThread().getId());
+                for (Long userId : mainUserList) {
+                    QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+                    // 无查询条件的查第一页，20条数据
+                    Page<User> userPage = userService.page(new Page<>(1, 20), queryWrapper);
+                    String redisKey = String.format("yupao:user:recommend:%s", userId);
+                    ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+                    // 写缓存
+                    try {
+                        valueOperations.set(redisKey, userPage, 30, TimeUnit.MINUTES);
+                    } catch (Exception e) {
+                        log.error("redis set key error", e);
+                    }
+                }
+            }
+        } catch (InterruptedException e) {
+            log.error("doCacheRecommendUser error", e);
+        } finally {
+            if (lock.isHeldByCurrentThread()) {
+                System.out.println("unLock: " + Thread.currentThread().getId());
+                lock.unlock();
+            }
+        }
+    }
+
+}
