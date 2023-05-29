e223fb49eba3cb81fc26589684e1a517aba47487
砂 流 <1018765366@qq.com>
2023-05-29 18:40:37 +0800

    第八期 add 测试类：测试分布式锁的看门狗机制是否续期成功

---
+++ src/test/java/com/yupi/yupao/service/RedisTest.java
@@ -0,0 +1,74 @@
+package com.yupi.yupao.service;
+
+import com.yupi.yupao.model.domain.User;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
+
+@SpringBootTest
+public class RedisTest {
+
+    @Resource
+    private RedisTemplate<String, java.io.Serializable> redisTemplate;
+
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Resource
+    private RedissonClient redissonClient;
+
+    @Test
+    void test() {
+        ValueOperations<String, java.io.Serializable> valueOperations = redisTemplate.opsForValue();
+        // 增
+        valueOperations.set("yupiString", "dog");
+        valueOperations.set("yupiInt", 1);
+        valueOperations.set("yupiDouble", 2.0);
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("yupi");
+        valueOperations.set("yupiUser", user);
+        // 查
+        Object yupi = valueOperations.get("yupiString");
+        // flip 避免空指针
+        Assertions.assertEquals("dog", (String) yupi);
+        yupi = valueOperations.get("yupiInt");
+        Assertions.assertEquals(1, (int) (Integer) yupi);
+        yupi = valueOperations.get("yupiDouble");
+        Assertions.assertEquals(2.0, (Double) yupi);
+        System.out.println(valueOperations.get("yupiUser"));
+
+        ValueOperations<String, String> stringStringValueOperations = stringRedisTemplate.opsForValue();
+        stringStringValueOperations.set("yupiS", "dog");
+        // redisTemplate.delete("yupiString");
+    }
+
+    @Test
+    void testWatchDog() {
+        RLock lock = redissonClient.getLock("yupao:precachejob:docache:lock");
+        try {
+            // 只有一个线程能获取到锁
+            if (lock.tryLock(0, -1, TimeUnit.MILLISECONDS)) {
+                // 延长到让 watchDog 续期
+                Thread.sleep(300_000);
+                System.out.println("getLock: " + Thread.currentThread().getId());
+            }
+        } catch (InterruptedException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            // 只能释放自己的锁
+            if (lock.isHeldByCurrentThread()) {
+                System.out.println("unLock: " + Thread.currentThread().getId());
+                lock.unlock();
+            }
+        }
+    }
+}
